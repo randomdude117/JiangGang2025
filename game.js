c77bd48fbf91ca8ac1306397058dadfaaeb1e5c4
randomdude117 <157393811+randomdude117@users.noreply.github.com>
2025-09-13 01:51:12 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,44 +1,42 @@
-const textElement = document.getElementById('text')
-const optionButtonsElement = document.getElementById('option-buttons')
+const gameTextElement = document.getElementById('game-text');
+const option1Button = document.getElementById('option1');
+const option2Button = document.getElementById('option2');
 
-let state = {}
-function startGame() {
-    state = {}
-    showTextNode(1)
-}
+let currentState = {
+    location: 'start',
+    inventory: []
+};
 
-function showTextNode(textNodeIndex) {
-    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-    textElement.innerText = textNode.text
-    while (optionButtonsElement.firstChild){
-        optionButtonsElement.removeChild(optionButtonsElement.firstChild)
-    }
-    
-}
-
-function selectOption(option) {
-
-}
-
-const textNodes = [
-    {
-        id: 1,
-        text: 'Wake up, need to choose.',
+const scenes = {
+    start: {
+        text: "Wake up, time to choose.",
         options: [
-            {
-                text: 'Go outside',
-                setState: {outside: true},
-                nextText: 2
-            },
-            {
-                text: 'Stay inside',
-                nextText: 2
-            }
+            { text: "Go outside", nextScene: 'outside' },
+            { text: "Stay inside", nextScene: 'inside' }
         ]
     },
-    {
-        id: 2
-    }
-]
+    outside: {
+        text: "Look like there is something up ahead.",
+        options: [
+            { text: "Go and see what is up ahead", nextScene: 'ahead' },
+            { text: "Go back inside", nextScene: 'backin' }
+        ]
+    },
+    inside: {
+        text:"filler text.",
+    },
 
-startGame()
+};
+
+function updateGame(sceneName) {
+    const currentScene = scenes[sceneName];
+    gameTextElement.textContent = currentScene.text;
+
+    option1Button.textContent = currentScene.options[0].text;
+    option1Button.onclick = () => updateGame(currentScene.options[0].nextScene);
+
+    option2Button.textContent = currentScene.options[1].text;
+    option2Button.onclick = () => updateGame(currentScene.options[1].nextScene);
+}
+
+updateGame('start'); // Initialize the game
